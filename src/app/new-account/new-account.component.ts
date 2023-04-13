840744f9b11f9a6d2999ad8051c85b2feadb5c8e
Sharada Kasam <sharadakasam@outlook.com>
2023-04-13 09:14:31 +0530

    provide service at appcomponent level, pass data

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,23 +1,27 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService]// we dont need to provide it here AccountService because it will 
+  //create a fresh new instance otherwise it will just get from the parent component in this case 
+  //it got used in appcomponent so we will get from there
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  constructor(private loggingService: LoggingService) {
+  constructor(private loggingService: LoggingService, private accountService: AccountService) {
 
   }
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.accountService.addAccount(accountName,accountStatus);
     this.loggingService.logStatusChange(accountStatus);
   }
 }
